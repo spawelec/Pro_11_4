6c7697bcddededc31ec2de085d4b94e817de6a7e
Sylwia <sylwia.pawelec@wp.pl>
2017-11-26 16:16:48 +0100

    New phone function

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function Phone( brand, price, color) {
+function Phone( brand, price, color, operatingSystem) {
     this.brand = brand;
     this.price = price;
     this.color = color;
+    this.operatingSystem = operatingSystem;
 }
 Phone.prototype.printInfo = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "." );
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price  + ". Operating system:" + this.operatingSystem);
 }
-var SamsungGalaxyS6 = new Phone("Samsung", 2500, "black");
-var iPhone6S = new Phone("Apple", 3500, "white");
-var OnePlusOne = new Phone("OnePlus", 1200, "silver")
+var SamsungGalaxyS6 = new Phone("Samsung", 2500, "black", "Android 7.0");
+var iPhone6S = new Phone("Apple", 3500, "white", "iOS");
+var OnePlusOne = new Phone("OnePlus", 1200, "silver", "Android 6.0")
 
 SamsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
